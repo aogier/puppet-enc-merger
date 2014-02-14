7abb08454ff4dc94ee2113afffb12638ce1dd801
aogier <aogier@2d4c9ed3-5f58-4833-8b86-e363d5d02457>
2014-02-14 17:35:34 +0000

    xmlrpcapiserver
    git-svn-id: https://devel.ieo.eu/svn/public/tools/ieo_enc/trunk@1074 2d4c9ed3-5f58-4833-8b86-e363d5d02457

--- enc/__init__.py
+++ enc/__init__.py
@@ -13,10 +13,51 @@ logging.basicConfig(format=FORMAT)
 logger = logging.getLogger('enc')
 logger.setLevel(logging.DEBUG)
 
-from local import xmlrpc_uri
+from local import xmlrpc_uri, remote
 wikiRoot = 'PuppetClasses'
 
+# plugin registration
 from enc.calendars import CalendarPlugin
+
+apiservers = []
+
+class ApiServer(object):
+
+    class __metaclass__(type):
+        '''
+        Maintain couple of global plugin registries.
+        
+        If called whitin Mafreader class creation, then update global
+        _fileFormats registry.
+        If called whitin a subclass creation, then update local
+        _decoders registry.
+        '''
+        def __new__(cls, name, bases, _dict):
+            _type = type.__new__(cls, name, bases, _dict)
+#             logger.debug('registering file format: %s' % name)
+            apiservers.append(_type)
+            return _type
+
+    def __init__(self, uri):
+        self.uri = uri
+    
+    def getNode(self, uri):
+        raise Exception, 'not implemented'
+
+class XmlRpcApiServer(ApiServer):
+    
+    __type__ = 'xmlrpc'
+    #fixme: it's a conf
+    __prefix__ = 'PuppetClasses'
+    
+    def __init__(self, uri):
+        super(XmlRpcApiServer, self).__init__(uri)
+        self.xmlrpc = xmlrpclib.ServerProxy(self.uri)
+    
+    def getNode(self, uri):
+        target = self.uri + uri
+        print 'getting', target
+        return self.xmlrpc.wiki.getPage(self.__prefix__ + uri)
 
 class IeoEnc(object):
 
@@ -34,25 +75,35 @@ class IeoEnc(object):
         logger.debug('called for node %s' % nodename)
         self.nodename = nodename
         domainParts = reversed(nodename.split('.'))
-        self.xmlrpc = xmlrpclib.ServerProxy(xmlrpc_uri)
-        uri = wikiRoot
+        
+#         self.xmlrpc = xmlrpclib.ServerProxy(xmlrpc_uri)
+        self.api = XmlRpcApiServer(remote['xmlrpc'])
+        
+#         uri = wikiRoot
+        uri = ''
         for domainPart in domainParts:
             uri = '/'.join([uri, domainPart])  
             logger.debug('merging classes from uri "%s"' % uri)
-            try:
-                page = self.xmlrpc.wiki.getPage(uri)
-            except:
-                continue
+#             try:
+                
+#                 page = self.xmlrpc.wiki.getPage(uri)
+            page = self.api.getNode(uri)
+                
+#             except:
+#                 print 'except !'
+#                 continue
             data = yaml.load(extract(page))
             for key in 'classes', 'parameters', 'environment':
                 if data.get(key):
                     self.data[key].update(data[key])
+        #FIXME: remove
         logger.debug(self.data)
 
         if self.data.get('classes'):
             for _class in self.data['classes']:
                 logger.debug('searching plugin for class %s ...' % _class)
                 for plugin in plugins:
+                    logger.debug('trying %s' % plugin)
                     if plugin.__puppet_class__ == _class:
                         logger.debug('... found %s' % plugin)
                         try:
@@ -67,6 +118,7 @@ class IeoEnc(object):
                             self.data['parameters'].update(parameters or {})
                         else:
                             self.data['parameters'] = parameters
+                        break
 
     def __repr__(self):
         return yaml.dump(dict(self.data),
